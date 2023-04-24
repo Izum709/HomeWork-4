b95c90a0b480cf0a824d8a3818edcefec244fa97
Alexandr Vinogradov <izum709@.com>
2023-04-24 10:12:28 +0300

    Add exemple 27

---
+++ Exemple_27/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    return(int.Parse(Console.ReadLine()));
+}
+
+int b = EnterData("Введите число a: ");
+int a = b;
+int i = 0;
+while (a>0)
+{
+    a = a/10;
+    i++;
+}
+Console.WriteLine(i);
+int sum = 0;
+int c=0;
+a=b;
+while ( c<=i )
+{
+    sum = sum + a%10; 
+    a /= 10;
+    c++;
+}
+Console.WriteLine(sum);
